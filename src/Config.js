13dad0b50506a7c7fa2a6c807a89fcf6a9176fa3
dbeccaria826 <beccdaniel@gmail.com>
2021-03-07 13:19:14 -0800

    Switched auth and config files to ts, seeing if these ts and js can be mixed in a way that works

--- src/Config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  appId: 'd81e44a1-c513-4d80-b9f5-93e4533a94dd',
-  redirectUri: 'http://localhost:3000',
-  scopes: [
-    'user.read',
-    'mailboxsettings.read',
-  ]
-}
