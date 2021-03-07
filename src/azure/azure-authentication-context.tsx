eb137171ba830ffa46f6a9cfd1e2270bc522aadd
dbeccaria826 <beccdaniel@gmail.com>
2021-03-07 14:48:19 -0800

    CheckIfWindowExists: has now been added to azure-authentication-context.tsx

--- src/azure/azure-authentication-context.tsx
+++ src/azure/azure-authentication-context.tsx
@@ -9,6 +9,15 @@ import {
 
 import { MSAL_CONFIG } from "./azure-authentication-config";
 
+const checkIfWindowExists = () => {
+  let location = null;
+  
+  if (window !== undefined) {
+    location = window.location.href;
+  }
+  return location;
+}
+
 export class AzureAuthenticationContext {
   private myMSALObj: PublicClientApplication = new PublicClientApplication(
     MSAL_CONFIG
@@ -36,7 +45,7 @@ export class AzureAuthenticationContext {
 
     this.loginRedirectRequest = {
       ...this.loginRequest,
-      redirectStartPage: window.location.href,
+      redirectStartPage: checkIfWindowExists(),
     };
   }
 
