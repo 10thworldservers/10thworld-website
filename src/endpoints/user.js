d284c77c9c0850f38c929a64bf1a4ba1746596a6
johnfischer14 <johndavidfischer@gmail.com>
2021-04-01 12:20:17 -0600

    added urls for user endpoints

--- src/endpoints/user.js
+++ src/endpoints/user.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 
 
-const restOptions = {
-    url: 'https://3085dedc0214.ngrok.io/api/CreateUpdateUser'
+
+
+function getUserById(Id) {
+    const restOptions = {
+        url: 'https://10thworldfunctions20210313113150.azurewebsites.net/api/GetUserById?code=ISGe5C3rDAAUGaVZattBpLaCeuebLIiKFiY4nRui2ILaiGSDQWe6QQ=='
+    }
+    //requires parameter for id and partition.
+    //partition should always be 'emailAddress'
+    
 }
 
-axios.post()
+///
+/// Creates or updates a user based on object passed in
+/// 
+function createUpdateUser(user) {
+    const restOptions = {
+        url: 'https://10thworldfunctions20210313113150.azurewebsites.net/api/CreateUpdateUser?code=qi2XLljGklKphBba2a16hfV3XbufbHh7KFMrXASR5JnSXCNi4tEhGQ=='
+    }
+}
