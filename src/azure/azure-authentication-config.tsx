bca792b8074e4ac7e945eee71ccdfd2056686776
dbeccaria826 <beccdaniel@gmail.com>
2021-04-02 12:51:37 -0700

    Added production and development env variables so our API keys don't get stolen

--- src/azure/azure-authentication-config.tsx
+++ src/azure/azure-authentication-config.tsx
@@ -3,7 +3,7 @@ import { Configuration, LogLevel } from "@azure/msal-browser";
 //testApp
 //const AzureActiveDirectoryAppClientId: string = '97e55fe7-ef1a-4892-ae76-59eba5fa4837';
 //10thWorldServers
-const AzureActiveDirectoryAppClientId: string = '7b9232dc-de44-48d6-b490-6a557913ecd2';
+const AzureActiveDirectoryAppClientId: string = process.env.GATSBY_AD_CLIENT_ID;
 
 
 //The current guidance from B2C is to use b2clogin.com as the authority. For example, $"https://{your-tenant-name}.b2clogin.com/tfp/{your-tenant-ID}/{policyname}"
@@ -12,8 +12,8 @@ const AzureActiveDirectoryAppClientId: string = '7b9232dc-de44-48d6-b490-6a55791
 export const MSAL_CONFIG: Configuration = {
   auth: {
     clientId: AzureActiveDirectoryAppClientId,
-    redirectUri: 'https://victorious-forest-0f1514b10.azurestaticapps.net',
-    authority: 'https://10thworldservers.b2clogin.com/tfp/10thworldservers.onmicrosoft.com/b2c_1_nicebeaver/'
+    redirectUri: process.env.GATSBY_REDIRECT_URI,
+    authority: process.env.GATSBY_AUTHORITY
     //validateAuthority: false
     //authority: 'https://10thworldservers.b2clogin.com/10thworldservers.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_nicebeaver'
 
