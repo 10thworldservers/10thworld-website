87666df660a1cb20fe966665eac960638277cccd
dbeccaria826 <beccdaniel@gmail.com>
2021-04-05 13:30:30 -0700

    Added copy to how-to-connect page, mobile styles, and images

--- src/pages/connect/index.js
+++ src/pages/connect/index.js
@@ -7,7 +7,15 @@ import { Container } from '../../components/global';
 import { ScrollProvider } from '../../context/ScrollProvider';
 import { HeaderProvider } from '../../context/HeaderProvider';
 import { AuthProvider } from '../../context/AuthProvider';
-import { InstContainer, InstTitle, InstText } from './style';
+import {
+  InstContainer,
+  InstTitle,
+  InstText,
+  InstFlexWrapper,
+  ImageWrapper,
+  FlexContainer
+} from './style';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Connect = () => {
   return (
@@ -17,35 +25,56 @@ const Connect = () => {
           <AuthProvider>
             <SEO title="About" />
             <Navigation />
-            <Container>
+            <FlexContainer>
               <InstContainer>
                 <InstTitle>
-                  Instructions
-        </InstTitle>
-                <InstText>
-                Get started by creating an account with us at 10th World Servers. 
-                Once signed up or signed in you can subscribe.
+                  How to Connect
+              </InstTitle>
+                <InstFlexWrapper>
+                  <InstText>
+                    Get started by creating an account with us at 10th World Servers.
+                    Once signed up or signed in you can subscribe.
                 </InstText>
-                <InstText>
-                Select Get Access to subscribe to our server plan.
-                Get Access will redirect to Stripe to complete payment. 
-                Once payment completes we'll redirect you back here to see your server details.
+                  <ImageWrapper>
+                    <StaticImage src='../../images/landscape-darkblue-lower.svg' alt="Blue landscape lower" />
+                  </ImageWrapper>
+
+                </InstFlexWrapper>
+                <InstFlexWrapper>
+                  <InstText>
+                    Clicking Get Access will give the option to subscribe to our server plan.
+                    You'll be redirected to Stripe to complete the transcation.
+                    Once payment completes we'll redirect you back here to see your server details.
                 </InstText>
-                <InstText>
-                Servers are pre-configured and linked instantly to your account. 
-                You'll receive a domain name, and port number for your Valheim server. 
-                After subscribing you'll be up and running instantly!
+                  <ImageWrapper>
+                    <StaticImage src='../../images/landscape-darkblue-upper.png' alt="Blue mountain landscape" />
+                  </ImageWrapper>
+                </InstFlexWrapper>
+                <InstFlexWrapper>
+                  <InstText>
+                    Servers are pre-configured and linked instantly to your account.
+                    You'll receive a domain name, and port number for your Valheim server.
+                    After subscribing you'll be up and running instantly!
                 </InstText>
-                <InstText>
-                We manage our servers with containers and kubernetes to maintain maximum uptime and durability.
-                You can control your server through our dashboard. 
-                See and set your default password.
-                Upload your personal world file. Save and backup your world here too.
-                Additional functionality is being added to the Dashboard for more personal control. 
-                For support email us directly at 10thworldsupport@gmail.com
+                  <ImageWrapper>
+                    <StaticImage src='../../images/mountain-and-sea-4770131_1920.jpg' alt="Mountains and sea" />
+                  </ImageWrapper>
+                </InstFlexWrapper>
+                <InstFlexWrapper>
+                  <InstText>
+                    We manage our servers with containers and kubernetes to maintain maximum uptime and durability.
+                    You can control your server through our dashboard,
+                    see and set your default password,
+                    and upload your personal world file. Save and backup your world here too.
+                    Additional functionality is being added to the Dashboard for more personal control.
+                    For support email us directly at 10thworldsupport@gmail.com
                 </InstText>
+                  <ImageWrapper>
+                    <StaticImage src='../../images/landscape-darkblue-upper-cloud.svg' alt="Server with cloud background" />
+                  </ImageWrapper>
+                </InstFlexWrapper>
               </InstContainer>
-            </Container>
+            </FlexContainer>
             <Footer />
           </AuthProvider>
         </Layout>
--- src/pages/connect/style.js
+++ src/pages/connect/style.js
@@ -1,13 +1,115 @@
 import styled from 'styled-components';
+import { Container } from '../../components/global';
 
+export const FlexContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
 export const InstContainer = styled.div`
-  margin-top: 12rem;
+  margin-top: 15vh;
+  display: flex;
+  flex-direction: column;
 `
 export const InstTitle = styled.h1`
   color: white;
-  margin-top: 12rem;
+  margin-top: 10vh;
+  margin-left: 5%;
+  @media (max-width: 575px) {
+    text-align: center;
+    margin-top: 10vh;
+    margin-bottom: 2.25rem;
+  }
+  @media (max-width: 767px) {
+    text-align: center;
+    margin-top: 10vh;
+  }
+  @media (max-width: 991px) {
+    margin-top: 10vh;
+  }
+`
+export const InstFlexWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1.75rem;
+  margin-bottom: 1.75rem;
+  justify-content: space-around;
+  align-items: center;
+  &:nth-child(odd) {
+    flex-direction: row-reverse;
+    img {
+    }
+  }
+
+  img {
+    height: 300px;
+    border-radius: 50%;
+    object-fit: center;
+  }
+
+  @media (max-width: 575px) {
+    display: flex;
+    flex-direction: column-reverse;
+    margin-top: 1.50rem;
+    margin-bottom: 0.50rem;
+    &:nth-child(odd) {
+      flex-direction: column-reverse;
+    }
+    img {
+      height: 150px;
+      border-radius: 10px;
+    }
+  }
+  @media (max-width: 767px) {
+    display: flex;
+    flex-direction: column-reverse;
+    margin-top: 0.50rem;
+    margin-bottom: 0.50rem;
+    &:nth-child(odd) {
+      flex-direction: column-reverse;
+    }
+    img {
+      border-radius: 10px;
+      /* max-height: 50%; */
+      height: 250px;
+    }
+  }
+  @media (max-width: 991px) {
+    img {
+      max-height: 75%;
+    }
+  }
+`
+export const ImageWrapper = styled.div`
+max-width: 30%;
+  @media (max-width: 575px) {
+    max-width: 100%;   
+  }
+  @media (max-width: 767px) {
+     max-width: 75%;
+  }
+  @media (max-width: 991px) {
+     
+  }
 `
 export const InstText = styled.p`
   color: white;
+  font-size: 1.25rem;
+  display: block;
+  margin-left: 1.25rem;
+  max-width: 50%;
+  line-height: 30px;
+
+  @media (max-width: 575px) {
+    max-width: 100%;
+    margin: 0;
+  }
+  @media (max-width: 767px) {
+   max-width: 75%;
+   margin: 0;
+  }
+  @media (max-width: 991px) {
+   
+  }
 `
 export default InstContainer;
