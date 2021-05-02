c4acc43213b2c23298e499817890c1267e738020
johnfischer14 <johndavidfischer@gmail.com>
2021-05-02 16:52:03 -0600

    Added production env variables. Changed azure config to use process.env vars.

--- src/azure/azure-authentication-config.js
+++ src/azure/azure-authentication-config.js
@@ -5,9 +5,9 @@ const AzureActiveDirectoryAppClientId = '7b9232dc-de44-48d6-b490-6a557913ecd2';
 
 export const MSAL_CONFIG = {
   auth: {
-    clientId: AzureActiveDirectoryAppClientId,
-    redirectUri: 'http://localhost:8000',
-    authority: 'https://10thworldservers.b2clogin.com/tfp/10thworldservers.onmicrosoft.com/b2c_1_nicebeaver/'
+    clientId: process.env.GATSBY_AD_CLIENT_ID,
+    redirectUri: process.env.GATSBY_REDIRECT_URI,
+    authority: process.env.GATSBY_AUTHORITY,
     //validateAuthority: false
     //authority: 'https://10thworldservers.b2clogin.com/10thworldservers.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_nicebeaver'
 
